99e7a1b25ddf42fe64021f43e2256320ba018d43
Saad Bhuiyan <ssaad00@stuy.edu>
2019-09-11 09:19:09 -0400

    change repo, move contents

---
+++ randomStudentFromKREWES.py
@@ -0,0 +1,30 @@
+KREWES = {
+	'orpheus': ['Emily', 'Kevin', 'Vishwaa', 'Eric', 'ray', 'Jesse', 'Tiffany', 'Amanda', 'Junhee', 'Jackie ', 'Tyler', 'Emory', 'Ivan', 'Elizabeth', 'Pratham', 'Shaw', 'Eric', 'Yaru', 'Kelvin', 'Hong Wei', 'Michael', 'Kiran', 'Amanda', 'Joseph', 'Tanzim', 'David', 'Yevgeniy'],
+	'rex': ['William', 'Joseph', 'Calvin', 'Ethan', 'Moody', 'Mo', 'Big Mo', 'Peihua', 'Saad', 'Benjamin', 'Justin', 'Alice', 'Hilary', 'Ayham', 'Michael', 'Matthew', 'Jionghao', 'Devin ', 'David', 'Jacob', 'Will', 'Hannah', 'Alex'],
+	'endymion': ['Grace', 'Nahi', 'Derek', 'Jun Tao', 'Connor', 'Jason', 'Tammy', 'Albert', 'Kazi', 'Derek', 'Brandon', 'Kenneth', 'Lauren', 'Biraj', 'Jeff', 'Jackson', 'Taejoon', 'Kevin', 'Jude', 'Sophie', 'Henry', 'Coby', 'Manfred', 'Leia', 'Ahmed', 'Winston']
+}
+
+'''
+print (KREWES)
+
+print (KREWES['orpheus'])
+print (KREWES['rex'])
+print (KREWES['endymion'])
+'''
+
+import random
+
+'''
+def randomStudent(team):
+    teamMembers = KREWES[team]
+    return teamMembers[random.randrange(len(teamMembers))]
+'''
+
+def randomStudent(team):
+    return random.choice(KREWES[team])
+
+print (randomStudent('orpheus'))
+
+print (randomStudent('rex'))
+
+print (randomStudent('endymion'))
