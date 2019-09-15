d24363ae9416e8fdd80aeb62529c52af0606c5d3
Saad Bhuiyan <ssaad00@stuy.edu>
2019-09-15 15:54:27 -0400

    made a dictionary and tested

--- 03_csv/csvToDictionary.py
+++ 03_csv/csvToDictionary.py
@@ -1,2 +1,12 @@
 import csv
 import random
+
+occupations = {}
+
+with open( "occupations.csv") as csv_file:
+    csv_reader = csv.reader( csv_file, delimiter = ",") 
+    for row in csv_reader:
+        occupations[ row[0]] = row[1]
+
+for occupation, percentage in occupations.items():
+    print( occupation, " - ", percentage)
