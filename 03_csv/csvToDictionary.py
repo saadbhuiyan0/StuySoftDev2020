e9f9a2f16905c9b6dc11ced3433e6ee0383c2eb1
Saad Bhuiyan <ssaad00@stuy.edu>
2019-09-16 16:58:32 -0400

    commens

--- 03_csv/csvToDictionary.py
+++ 03_csv/csvToDictionary.py
@@ -3,10 +3,15 @@ import random
 
 occupations = {}
 
-with open( "occupations.csv") as csv_file:
+with open( "occupations.csv") as csv_file: #csv to dictionary
     csv_reader = csv.reader( csv_file, delimiter = ",") 
     for row in csv_reader:
-        occupations[ row[0]] = row[1]
+        occupations[ row[0]] = float(row[1]) #input should be float
+    del occupations[ "Job Class"] #delete header
+    del occupations[ "Total"] #delete footer
 
-for occupation, percentage in occupations.items():
-    print( occupation, " - ", percentage)
+#for occupation, percentage in occupations.items():
+    #print( occupation, " - ", percentage)
+
+def randomOccupation():
+    
