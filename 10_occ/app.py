d6e7357dc26e05ace82c384cfe41116dc0db6a99
Saad Bhuiyan <ssaad00@stuy.edu>
2019-09-20 17:00:17 -0400

    everything prints as expected

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -1,24 +1,48 @@
 #teamFakeMoonLanding_bhuiyanS_huangT
 #SoftDev1 pd2
-#K9 - Classwork working with static folder,templates, and html
-#2019-09-19
+#K10 -- Using Flask and Jinja2 to present occupations.csv as a table in HTML
+#2019-09-20
+
 
 from flask import Flask, render_template
+import csv, random
 app = Flask(__name__)
 
 @app.route("/")
-def Hello_World():
-    print(__name__)
-    return "Hello World!"
-
-@app.route("/my_foist_template")
-def usingTemplate():
-    print("my_foist_template")
-    coll = {0, 1, 2, 3, 4, 5}
+def homePage():
+    print("moon landing")
+    coll = {"the moon landing was fake", "okay maybe it was real", "but birds definitely aren't real"}
     return render_template( 
         'my_foist_template.html',
         foo = "teamFakeMoonLanding", 
         collection = coll)
+
+occupations = {}
+jobClass = []
+percentage = []
+
+with open( "occupations.csv") as csv_file: #csv to dictionary
+    csv_reader = csv.reader( csv_file, delimiter = ",") 
+    for row in csv_reader:
+        occupations[ row[0]] = row[1] 
+    del occupations[ "Job Class"] #delete header
+    del occupations[ "Total"] #delete footer
+
+def randomOccupation():
+    for occupation in occupations:
+        jobClass.append( occupation)
+        percentage.append( float( occupations[ occupation])) #percentage should be float
+    return random.choices( jobClass, weights = percentage, k = 1)
+
+@app.route("/occupyflaskt")
+def occupationscsvToHTML():
+    print("occupations csv to html")
+    return render_template( 
+        'occupations_csv_to_HTML_table.html',
+        randomOccupation = randomOccupation(),
+        dictionary = zip(jobClass, percentage)
+    )
+
 
 if __name__ == "__main__":
     app.debug = True
