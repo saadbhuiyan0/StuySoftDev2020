21be8f307ae7daf0b15918724c91eef70f63384c
Saad Bhuiyan <ssaad00@stuy.edu>
2019-09-23 09:22:46 -0400

    added comments, fixed bugs

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -17,30 +17,34 @@ def homePage():
         foo = "teamFakeMoonLanding", 
         collection = coll)
 
-occupations = {}
-jobClass = []
-percentage = []
+occupations = {} #the dictionary where we will input the values from the csv file
+#these lists are used in two different functions so they are global
+jobClass = [] #list of keys (jobs) from occupations dictionary
+percentage = [] #list of values (percentages) from occupations dictionary
 
-with open( "occupations.csv") as csv_file: #csv to dictionary
-    csv_reader = csv.reader( csv_file, delimiter = ",") 
-    for row in csv_reader:
-        occupations[ row[0]] = row[1] 
+with open( "occupations.csv") as csv_file: #opens the csv file to read in python
+    csv_reader = csv.reader( csv_file, delimiter = ",") #csv reader function using "," as the delimiter
+    for row in csv_reader: #for loop iterates through each line of the csv file
+        occupations[ row[0]] = row[1] #sets the first part of the line as the key, and second part as the value in the dictionary
     del occupations[ "Job Class"] #delete header
     del occupations[ "Total"] #delete footer
+    for occupation in occupations: #for loop to iterate through each job in occupations dictionary
+        jobClass.append( occupation) #adds the job to the list jobClass
+        percentage.append( float( occupations[ occupation])) #adds the percentage to list of percentage after casting to float
 
 def randomOccupation():
-    for occupation in occupations:
-        jobClass.append( occupation)
-        percentage.append( float( occupations[ occupation])) #percentage should be float
-    return random.choices( jobClass, weights = percentage, k = 1)
+    return random.choices( jobClass, weights = percentage, k = 1) [0] #prints a random occupation using percentages as weights
 
 @app.route("/occupyflaskt")
 def occupationscsvToHTML():
     print("occupations csv to html")
     return render_template( 
         'occupations_csv_to_HTML_table.html',
+        teamName = "Team Fake Moon Landing",
+        teamMembers = "Saad Bhuiyan and Tyler Huang",
+        description = "We used Python, Flask, Jinja2, and HTML to take a data set (occupations.csv) and print the data in a table form.",
         randomOccupation = randomOccupation(),
-        dictionary = zip(jobClass, percentage)
+        dictionary = zip(jobClass, percentage) #to be able to loop through two different lists using jinja2, we had to zip them together
     )
 
 
