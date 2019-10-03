698d69b496052c08185b5df03ce5d605640dc14c
Saad Bhuiyan <ssaad00@stuy.edu>
2019-10-03 10:26:26 -0400

    error

--- 15_login/app.py
+++ 15_login/app.py
@@ -1,42 +1,53 @@
-#Saad Bhuiyan and Benjamin Avrahami
+#Saad Bhuiyan and Coby Sontag
 #SoftDev1 pd2
-#K12 -- Using Python, Flask, Jinja2, and HTML to create a form and custom response.
-#2019-09-26 
+#K15 -- Create a user log in experience
+#2019-10-02
 
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, url_for
 app = Flask(__name__)
 
-teamName = "Team Cod"
-teamMembers = "Saad Bhuiyan and Coby Sontag"
+correctUsername = "teamCod"
+correctPassword = "fishAndChips"
+loggedIn = False
 
 @app.route("/")
 def root():
-    print(app)
-    return render_template("login.html", 
-                            teamName = teamName,
-                            teamMembers = teamMembers
-    )
+    if loggedIn:
+        print("redirect to /welcome from /")
+        return redirect(url_for("welcome"))
+    else:
+        print("redirect to /login from /")
+        return redirect(url_for("login"))
 
-myUsername = "teamCod"
-myPassword = "fishy"
+@app.route("/welcome")
+def welcome():
+    print("welcome page")
+    return render_template("welcome.html",
+                            username = correctUsername
+    )
 
 @app.route("/login")
-def authenticate():
-    if request.form["username"] == myUsername and request.form["password"] == myPassword
-        render_template("welcome.html",
-                        username = myUsername)
-    else
-        return render_template("response.html", 
-                            teamName = teamName,
-                            teamMembers = teamMembers,
-                            name = request.args['name'],
-                            donation = request.args['donation'],
-                            method = request.method
-    )
+def login():
+    print("log in page")
+    return render_template("login.html")
+
+@app.route("/auth")
+def auth():
+    print("authentication")
+    if request.form["username"] == correctUsername:
+        if request.form["password"] == correctPassword:
+            loggedIn = True
+            return redirect(url_for("welcome"))
+        else:
+            return "wrong password"
+    else: 
+        return "wrong username"
 
 @app.route("/logout")
 def logout():
-    return render_template("logout.html")
+    print("log out page")
+    loggedIn = False
+    return "Thanks for visiting!"
 
 if __name__ == "__main__":
     app.debug = True
