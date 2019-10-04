73ed5e94462774c8bcdb190dc5f5ca5c8444bab3
Saad Bhuiyan <ssaad00@stuy.edu>
2019-10-04 14:06:54 -0400

    blank pages :(

--- 16_flasherit/app.py
+++ 16_flasherit/app.py
@@ -1,9 +1,9 @@
 #Saad Bhuiyan and Coby Sontag
 #SoftDev1 pd2
-#K15 -- Create a user log in experience
-#2019-10-02
+#K16 -- Add flashed messages to 15_login and use Jinja inheritance
+#2019-10-04
 
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 app = Flask(__name__)
 
 app.secret_key = "iDontLikeFish"
@@ -27,6 +27,7 @@ def root():
 
 @app.route("/welcome")
 def welcome():
+    #flash("You're logged in! Welcome!")
     print("welcome page")
     return render_template("welcome.html",
                             username = session["username"],
@@ -36,6 +37,7 @@ def welcome():
 
 @app.route("/login")
 def login():
+    #flash("Please fill in your login credentials.")
     print("login page")
     return render_template("login.html",
                             teamName = teamName,
@@ -52,16 +54,19 @@ def auth():
             print("valid login redirect to /welcome from /auth")
             return redirect(url_for("welcome"))
         else:
+            #flash("Sorry that's the wrong password.")
             session["incorrect"] = "password"
             print("invalid password redirect to /invalidLogin from /auth")
             return redirect(url_for("invalidLogin"))
     else: 
+        #flash("Sorry that's the wrong username.")
         session["incorrect"] = "username"
         print("invalid username redirect to /invalidLogin from /auth")
         return redirect(url_for("invalidLogin"))
 
 @app.route("/invalidLogin")
 def invalidLogin():
+    #flash("Sorry your login information was invalid.")
     print("invalid login")
     return render_template("invalidLogin.html",
                             teamName = teamName,
@@ -71,6 +76,7 @@ def invalidLogin():
 
 @app.route("/logout")
 def logout():
+    #flash("Good bye! Please come back...")
     print("log out page")
     session["loginStatus"] = False
     return render_template("logout.html",
