f0d4417564a6ae9b24e8ecdb3eaa1394587fa9a7
Saad Bhuiyan <ssaad00@stuy.edu>
2019-12-10 09:52:04 -0500

    add comments and randomStudent()

--- 27_js/funky_town.js
+++ 27_js/funky_town.js
@@ -1,3 +1,4 @@
+// factorial(n) returns the factorial of the input number
 var factorial = function(n) {
     if (n > 1) {
         return n * factorial(n - 1);
@@ -7,6 +8,8 @@ var factorial = function(n) {
     }
 }
 
+
+// fibonacci(n) returns the nth number in the fibonacci sequence
 var fibonacci = function(n) {
     if (n == 0) {
         return 0;
@@ -17,4 +20,22 @@ var fibonacci = function(n) {
     else {
         return fibonacci(n - 1) + fibonacci(n - 2)
     }
+}
+
+
+// gcd(a,b) returns the greatest common divisor of the input numbers a and b
+var gcd = function(a,b) {
+    return 0;
+}
+
+
+// array of students in SoftDev period 2
+var studentsInPeriod2 = ["abedinM", "avrahamiB", "bhuiyanS", "caoW", "chanM", "chenE", "chenJ", "friedH", 
+                         "huangP", "huangT", "linD", "linW", "lupeaD", "mosesB", "niA", "olinJ", "olteanuA", 
+                         "rahmanM", "sontagC", "thompsonA", "wangD", "wuJ", "yusufovJ", "zenH"];
+
+// randomStudent() returns a random student from the array studentsInPeriod2
+var randomStudent = function() {
+    randomNumber = Math.floor(Math.random() * studentsInPeriod2.length)
+    return studentsInPeriod2[randomNumber]
 }
