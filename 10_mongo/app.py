08fc327e450cfe3c3b0f0c92a0c8781fcc27c3ae
Saad Bhuiyan <ssaad00@stuy.edu>
2020-03-01 04:09:24 -0500

    implement balldontlie api

---
+++ 10_mongo/app.py
@@ -0,0 +1,38 @@
+# Saad Bhuiyan and Jackson Zou
+# SoftDev2 pd9
+# K06 -- Canvas
+# 2020-02-11
+
+
+from pymongo import MongoClient
+from bson.json_util import loads
+import json
+import urllib.request
+
+
+def api_data_for_page(page):
+    api_call = urllib.request.urlopen("https://www.balldontlie.io/api/v1/players?per_page=100&page=" + str(page))
+    response = api_call.read()
+    response_data = json.loads(response)
+    players = response_data["data"]
+    return players
+
+def api_data_json():
+    with open("data.json", "w") as f:
+        data = {"players":[]}
+        players = data["players"]
+        for page in range(1,33):
+            api_data = api_data_for_page(page)
+            for player in range(100):
+                players.append(api_data[player])
+        api_data = api_data_for_page(33)
+        for player in range(66):
+            players.append(api_data[player])
+        json.dump(data, f)
+
+try:
+    f = open("data.json")
+except FileNotFoundError:
+    api_data_json()
+
+
