4af17bf71e90d39793c69bb01f5edf0e3ffe1450
Saad Bhuiyan <ssaad00@stuy.edu>
2020-03-07 22:59:37 -0500

    updates

--- 11_mongoflask/main.py
+++ 11_mongoflask/main.py
@@ -17,9 +17,9 @@ from api import api_data_to_json
 from parse import data_to_db, get_players_by_first_name, get_players_by_last_name, get_players_by_position, get_players_by_team_name, get_players_by_division, get_players_by_conference
 
 
-client = pymongo.MongoClient('localhost', 27017) # port 27017
-db = client['teamname'] # does not have to exist
-col = db['nba']
+client = MongoClient('localhost', 27017) # port 27017
+db = client['nba'] # does not have to exist
+col = db['balldontlie']
 
 
 if col.count() == 0:
@@ -28,9 +28,9 @@ if col.count() == 0:
 	data_to_db(col, "balldontlie.json")
 
 
-# get_players_by_first_name("Mitchell", col)
+get_players_by_first_name("Mitchell", col)
 # get_players_by_last_name("Robinson", col)
 # get_players_by_position("C", col)
-get_players_by_team_name("New York Knicks", col)
+# get_players_by_team_name("New York Knicks", col)
 # get_players_by_division("Atlantic", col)
 # get_players_by_conference("East", col)
